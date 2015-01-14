17d94e5b6772b390a509ca7dae27e220b716f9ce
Flavio Percoco <flaper87@gmail.com>
2015-01-14 12:59:58 +0100

    Add a highfive package and a setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='highfive',
+      version='0.1',
+      description='Github hooks to provide an encouraging atmosphere for new contributors',
+      author='Rust Community',
+      author_email='no-idea@no-server.no-suffix',
+      url='https://github.com/nick29581/highfive',
+      packages=['highfive'],
+)

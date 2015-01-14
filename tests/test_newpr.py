189ce075749fe330139080acfbae1a6be6277e73
Flavio Percoco <flaper87@gmail.com>
2015-01-14 13:32:21 +0100

    Move json files

--- tests/test_newpr.py
+++ tests/test_newpr.py
@@ -20,5 +20,14 @@ class TestNewPR(BaseTest):
         submodule_diff = self._load_fake('submodule.diff')
         self.assertTrue(newpr.modifies_submodule(submodule_diff))
 
-        submodule_diff = self._load_fake('normal.diff')
-        self.assertFalse(newpr.modifies_submodule(submodule_diff))
+        normal_diff = self._load_fake('normal.diff')
+        self.assertFalse(newpr.modifies_submodule(normal_diff))
+
+
+    def test_choose_reviewer(self):
+        normal_diff = self._load_fake('normal.diff')
+        reviewer = newpr.choose_reviewer('rust',
+                                         'rust-lang',
+                                         normal_diff,
+                                         'nikomatsakis')
+        self.assertNotEqual('nikomatsakis', reviewer)

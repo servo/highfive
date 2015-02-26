be0acc609d29c02f3577a80a634d7b1ae86b524c
Nick Cameron <ncameron@mozilla.com>
2015-02-27 09:46:28 +1300

    s/nick29581/nrc

--- highfive/newpr.py
+++ highfive/newpr.py
@@ -25,7 +25,7 @@ collabo_url = "https://api.github.com/repos/%s/%s/collaborators"
 issue_url = "https://api.github.com/repos/%s/%s/issues/%s"
 
 welcome_with_reviewer = '@%s (or someone else)'
-welcome_without_reviewer = "@nick29581 or @huonw (NB. this repo may be misconfigured)"
+welcome_without_reviewer = "@nrc or @huonw (NB. this repo may be misconfigured)"
 raw_welcome = """Thanks for the pull request, and welcome! The Rust team is excited to review your changes, and you should hear from %s soon.
 
 If any changes to this PR are deemed necessary, please add them as extra commits. This ensures that the reviewer can see what has changed since they last reviewed the code. The way Github handles out-of-date commits, this should also make it reasonably obvious what issues have or haven't been addressed. Large or tricky changes may require several passes of review and changes.
@@ -180,7 +180,7 @@ def find_reviewer(commit_msg):
 
 # Choose a reviewer for the PR
 def choose_reviewer(repo, owner, diff, exclude):
-    if not (owner == 'rust-lang' or (owner == 'nick29581' and repo == 'highfive')):
+    if not (owner == 'rust-lang' or (owner == 'nrc' and repo == 'highfive')):
         return 'test_user_selection_ignore_this'
 
     # Get JSON data on reviewers.

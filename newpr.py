31d377fce8cfd6e9fa04d1993f72ebedd1e464b6
Josh Matthews <josh@joshmatthews.net>
2015-04-08 10:28:09 -0400

    Add automatic labelling.

--- newpr.py
+++ newpr.py
@@ -12,6 +12,53 @@ from StringIO import StringIO
 import gzip
 cgitb.enable()
 
+def api_req(method, url, data=None, username=None, token=None, media_type=None):
+    data = None if not data else json.dumps(data)
+    headers = {} if not data else {'Content-Type': 'application/json'}
+    req = urllib2.Request(url, data, headers)
+    req.get_method = lambda: method
+    if token:
+        base64string = base64.standard_b64encode('%s:%s' % (username, token)).replace('\n', '')
+        req.add_header("Authorization", "Basic %s" % base64string)
+
+    if media_type:
+        req.add_header("Accept", media_type)
+    f = urllib2.urlopen(req)
+    if f.info().get('Content-Encoding') == 'gzip':
+        buf = StringIO(f.read())
+        f = gzip.GzipFile(fileobj=buf)
+    return f.read()
+
+def post_comment(body, owner, repo, issue, user, token):
+    post_comment_url = "https://api.github.com/repos/%s/%s/issues/%s/comments"
+    try:
+        result = api_req("POST", post_comment_url % (owner, repo, issue), {"body": body}, user, token)
+    except urllib2.HTTPError, e:
+        if e.code == 201:
+            pass
+	else:
+            raise e
+
+def add_label(label, owner, repo, issue, user, token):
+    add_label_url = "https://api.github.com/repos/%s/%s/issues/%s/labels"
+    try:
+        result = api_req("POST", add_label_url % (owner, repo, issue), label, user, token)
+    except urllib2.HTTPError, e:
+        if e.code == 201:
+            pass
+	else:
+            raise e
+
+def remove_label(label, owner, repo, issue, user, token):
+    remove_label_url = "https://api.github.com/repos/%s/%s/issues/%s/labels/%s"
+    try:
+        result = api_req("DELETE", remove_label_url % (owner, repo, issue label), {}, user, token)
+    except urllib2.HTTPError, e:
+        if e.code == 201:
+            pass
+	else:
+            raise e
+
 def is_new_contributor(username, stats):
     for contributor in stats:
         if contributor['login'] == username:
@@ -20,30 +67,12 @@ def is_new_contributor(username, stats):
 
 contributors_url = "https://api.github.com/repos/%s/%s/contributors?per_page=400"
 collaborators_url = "https://api.github.com/repos/%s/%s/collaborators"
-post_comment_url = "https://api.github.com/repos/%s/%s/issues/%s/comments"
 
 welcome_msg = "Thanks for the pull request, and welcome! The Servo team is excited to review your changes, and you should hear from @%s (or someone else) soon."
 warning_summary = '<img src="http://www.joshmatthews.net/warning.svg" alt="warning" height=20> **Warning** <img src="http://www.joshmatthews.net/warning.svg" alt="warning" height=20>\n\n%s'
 unsafe_warning_msg = 'These commits modify **unsafe code**. Please review it carefully!'
 reftest_required_msg = 'These commits modify layout code, but no reftests are modified. Please consider adding a reftest!'
 smoketest_required_msg = '@%s, please confirm that src/test/html/acid1.html and your favourite wikipedia page still render correctly!'
-
-def api_req(method, url, data=None, username=None, token=None, media_type=None):
-	data = None if not data else json.dumps(data)
-	headers = {} if not data else {'Content-Type': 'application/json'}
-	req = urllib2.Request(url, data, headers)
-	req.get_method = lambda: method
-	if token:
-        	base64string = base64.standard_b64encode('%s:%s' % (username, token)).replace('\n', '')
-        	req.add_header("Authorization", "Basic %s" % base64string)
-
-	if media_type:
-		req.add_header("Accept", media_type)
-	f = urllib2.urlopen(req)
-	if f.info().get('Content-Encoding') == 'gzip':
-	    buf = StringIO(f.read())
-	    f = gzip.GzipFile(fileobj=buf)
-	return f.read()
 
 print "Content-Type: text/html;charset=utf-8"
 print
@@ -56,26 +85,27 @@ token = config.get('github', 'token')
 post = cgi.FieldStorage()
 payload_raw = post.getfirst("payload",'')
 payload = json.loads(payload_raw)
-if payload["action"] != "opened":
-	sys.exit(0)
 
 owner = payload['pull_request']['base']['repo']['owner']['login']
 repo = payload['pull_request']['base']['repo']['name']
+issue = str(payload["number"])
+
+if payload["action"] in ["synchronized", "opened"]:
+    remove_label("S-awaiting merge", owner, repo, issue, user, token)
+    remove_label("S-tests failed", owner, repo, issue, user, token)
+    remove_label("S-needs code changes", owner, repo, issue, user, token)
+    add_label("S-needs review", owner, repo, issue, user, token)
+
+if payload["action"] == "synchronized" and payload['pull_request']['mergeable']:
+    remove_label("S-needs rebase", owner, repo, issue, user, token)
+
+if payload["action"] != "opened":
+    sys.exit(0)
+
 stats_raw = api_req("GET", contributors_url % (owner, repo), None, user, token)
 stats = json.loads(stats_raw)
 
 author = payload["pull_request"]['user']['login']
-issue = str(payload["number"])
-
-def post_comment(body, owner, repo, issue, user, token):
-    global post_comment_url
-    try:
-        result = api_req("POST", post_comment_url % (owner, repo, issue), {"body": body}, user, token)
-    except urllib2.HTTPError, e:
-        if e.code == 201:
-                pass
-	else:
-		raise e
 
 if is_new_contributor(author, stats):
         #collaborators = json.load(urllib2.urlopen(collaborators_url))

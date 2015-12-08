9a2fcba75f9c8160463fb05e55ffc62719ff8deb
Yoong Kang Lim <yoongkang.lim@gmail.com>
2015-12-09 00:55:45 +1100

    First refactor MissingTestHandler

--- handlers/missing_test/__init__.py
+++ handlers/missing_test/__init__.py
@@ -7,14 +7,23 @@ class MissingTestHandler(EventHandler):
         diff = api.get_diff()
         layout_changed = False
         for line in diff.split('\n'):
-            if line.startswith('diff --git') and line.find('components/layout/') > -1:
-                layout_changed = True
-            if line.startswith('diff --git') and \
-               (line.find('tests/ref') > -1 or line.find('tests/wpt') > -1):
+            if self.tests_present(line):
                 return
+            if self.layout_changed(line):
+                layout_changed = True
 
         if layout_changed:
             self.warn(reftest_required_msg)
 
+    def tests_present(self, line):
+        return (
+            line.startswith('diff --git') and
+            (line.find('tests/ref') > -1
+                or line.find('tests/wpt') > -1))
+
+    def layout_changed(self, line):
+        return (
+            line.startswith('diff --git') and
+            line.find('components/layout/') > -1)
 
 handler_interface = MissingTestHandler

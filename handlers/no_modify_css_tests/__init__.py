f029ea690bc0fac52f7f2e169e7d564816ac41b0
qpid <sauravsachidanand@gmail.com>
2016-02-02 17:52:30 +0530

    Added break statement after finding first occurance of 'tests/wpt/css-tests', and changed warning message.

--- handlers/no_modify_css_tests/__init__.py
+++ handlers/no_modify_css_tests/__init__.py
@@ -1,8 +1,8 @@
 from eventhandler import EventHandler
 
 NO_MODIFY_CSS_TESTS_MSG = '''This pull request modifies the contents of
-'tests/wpt/css-tests/', which are overwriten when the directory is synced
-from upstream occasionally.'''
+'tests/wpt/css-tests/', which are overwriten occasionally whenever the
+directory is synced from upstream.'''
 
 class NoModifyCSSTestsHandler(EventHandler):
     DIR_TO_CHECK = "tests/wpt/css-tests"
@@ -11,5 +11,6 @@ class NoModifyCSSTestsHandler(EventHandler):
         for line in api.get_diff().split('\n'):
             if self.DIR_TO_CHECK in line:
                 self.warn(NO_MODIFY_CSS_TESTS_MSG)
+                break
 
 handler_interface = NoModifyCSSTestsHandler

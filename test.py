85b0567a367855ab9e487f967c19d064b1198857
Paul Padier <pp_online@outlook.com>
2016-04-02 20:19:44 +0900

    Add get_pull API method

--- test.py
+++ test.py
@@ -5,13 +5,14 @@ import sys
 import traceback
 
 class TestAPIProvider(APIProvider):
-    def __init__(self, payload, user, new_contributor, labels, assignee, diff=""):
+    def __init__(self, payload, user, new_contributor, labels, assignee, diff="", pull_request=""):
         APIProvider.__init__(self, payload, user)
         self.new_contributor = new_contributor
         self.comments_posted = []
         self.labels = labels
         self.assignee = assignee
         self.diff = diff
+        self.pull_request = pull_request
 
     def is_new_contributor(self, username):
         return self.new_contributor
@@ -31,6 +32,9 @@ class TestAPIProvider(APIProvider):
     def get_diff(self):
         return self.diff
 
+    def get_pull(self):
+        return self.pull_request
+
     def set_assignee(self, assignee):
         self.assignee = assignee
 
@@ -43,6 +47,7 @@ def create_test(filename, initial, expected):
     initial_values = {'new_contributor': initial.get('new_contributor', False),
                       'labels': initial.get('labels', []),
                       'diff': initial.get('diff', ''),
+                      'pull_request': initial.get('pull_request', ''),
                       'assignee': initial.get('assignee', None)}
     return {'filename': filename,
             'initial': initial_values,
@@ -59,7 +64,7 @@ def run_tests(tests):
             payload = get_payload(test['filename'])['payload']
             initial = test['initial']
             api = TestAPIProvider(payload, 'highfive', initial['new_contributor'], initial['labels'],
-                                  initial['assignee'], initial['diff'])
+                                  initial['assignee'], initial['diff'], initial['pull_request'])
             handle_payload(api, payload)
             expected = test['expected']
             if 'comments' in expected:

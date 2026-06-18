87ab410788013baf49cedf55d5ecf86d0594856d
Josh Matthews <josh@joshmatthews.net>
2026-06-18 07:37:56 +0200

    Change language.
    Signed-off-by: Josh Matthews <josh@joshmatthews.net>

--- handlers/monthly_update/__init__.py
+++ handlers/monthly_update/__init__.py
@@ -1,8 +1,8 @@
 from eventhandler import EventHandler
 
-MSG = ('We would like to write about this change in the monthly blog post! '
-       'We have a few questions about this PR that will make this task '
-       'easie :smile:\n\n'
+MSG = ('Someone thinks this change could be added to the monthly blog '
+       'post! To help with this, we need someone to answer the following '
+       'qurstions: :smile:\n\n'
        '1. Who is most impacted by this change: users, '
        'Servo developers, embedders, or some other group?\n'
        '1. What observable difference does this change make?\n'
@@ -12,13 +12,16 @@ MSG = ('We would like to write about this change in the monthly blog post! '
        'If this change is part of a broader feature/project please '
        'make sure the PR description contains a `Fixes: #12345` or '
        '`Part of: #12345` issue reference.\n\n'
+       'Please add `@%s monthly update` when answering these '
+       'questions so the bot notices your answer (or just quote this '
+       'comment).\n\n'
        'Thanks for helping us prepare the monthly blog post! :heart:')
 
 
 class MonthlyUpdateHandler(EventHandler):
     def on_issue_labeled(self, api, payload):
         if payload['label']['name'].lower() == 'monthly update':
-            api.post_comment(MSG)
+            api.post_comment(MSG % api.user)
 
 
 handler_interface = MonthlyUpdateHandler

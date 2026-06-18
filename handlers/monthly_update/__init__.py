f0fdf957b86e256ccb7ee1b2088c565ef6ed7898
Josh Matthews <josh@joshmatthews.net>
2026-06-18 08:07:41 +0200

    Add automation for noticing replies.
    Signed-off-by: Josh Matthews <josh@joshmatthews.net>

--- handlers/monthly_update/__init__.py
+++ handlers/monthly_update/__init__.py
@@ -1,5 +1,11 @@
 from eventhandler import EventHandler
 
+import re
+
+REPLY = "monthly update"
+
+REACTION = "eyes"
+
 MSG = ('Someone thinks this change could be added to the monthly blog '
        'post! To help with this, we need someone to answer the following '
        'qurstions: :smile:\n\n'
@@ -12,7 +18,7 @@ MSG = ('Someone thinks this change could be added to the monthly blog '
        'If this change is part of a broader feature/project please '
        'make sure the PR description contains a `Fixes: #12345` or '
        '`Part of: #12345` issue reference.\n\n'
-       'Please add `@%s monthly update` when answering these '
+       'Please add `@%s %s` when answering these '
        'questions so the bot notices your answer (or just quote this '
        'comment).\n\n'
        'Thanks for helping us prepare the monthly blog post! :heart:')
@@ -21,7 +27,20 @@ MSG = ('Someone thinks this change could be added to the monthly blog '
 class MonthlyUpdateHandler(EventHandler):
     def on_issue_labeled(self, api, payload):
         if payload['label']['name'].lower() == 'monthly update':
-            api.post_comment(MSG % api.user)
+            api.post_comment(MSG % (api.user, REPLY))
+
+    def on_new_comment(self, api, payload):
+        if payload['issue']['state'] != 'open':
+            return
+
+        user = payload['comment']['user']['login']
+        if user == api.user:    # ignore comments from self
+            return              # (since `MSG` already has `REPLY`)
+
+        msg = payload['comment']['body']
+
+        if re.search(r'@%s[: ]*%s' % (api.user, REPLY), str(msg)):
+            api.add_reaction(payload['comment']['id'], REACTION)
 
 
 handler_interface = MonthlyUpdateHandler
